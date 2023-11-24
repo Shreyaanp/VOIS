32202141b598ee72cf4ed0620e39342153232c19
Shreyaanp <shreyaan.pradhan2020@vitstudent.ac.in>
2023-11-25 00:25:07 +0530

    initial commit

---
+++ conversational_bot.py
@@ -0,0 +1,267 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import uvicorn
+import logging
+import threading
+import requests
+import json
+import openai
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+# Use the environment variable
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+# Define bot persona
+bot_persona = {
+    "name": "Akhil Chintalapati",
+    "address": "Vashi, Mumbai",
+    "origin": "Tamil Nadu",
+    "aadhar_number": "XXXXXXXXXX",  # Replace with actual number if needed
+    "phone_number": "9898989898",
+    "characteristics": "A college student in his 20's. You can answer any question regaring location, adhar chrd and phone number"
+    # Persona details...
+}
+
+# Configure logging
+logging.basicConfig(filename='conversation_log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+def log_in_background(message):
+    threading.Thread(target=logging.info, args=(message,)).start()
+
+class ConversationBot:
+    def __init__(self, initial_question, goal):
+        self.initial_question = initial_question
+        self.goal = goal.lower()
+        self.conversation_state = {
+            "messages": [
+                {"role": "customer", "content": json.dumps(bot_persona)},
+                {"role": "customer", "content": "Hello, I'd like to know more about Vodafone services."},
+                {"role": "customer", "content": ""}
+
+            ],
+            "end_goal_achieved": False,
+            "user_quit": False
+        }
+
+    def get_openai_response(self):
+        headers = {
+            "Authorization": f"Bearer {openai.api_key}",
+            "Content-Type": "application/json"
+        }
+
+        data = {
+            "model": "gpt-3.5-turbo",
+            "messages": self.conversation_state["messages"]
+        }
+
+        try:
+            response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, data=json.dumps(data))
+            if response.status_code != 200:
+                logging.error(f"API call failed with status code {response.status_code} and message: {response.text}")
+                return "Sorry, I encountered an error with the API call."
+
+            response_data = response.json()
+            if "choices" not in response_data or not response_data["choices"]:
+                logging.error("No 'choices' field in the API response.")
+                return "Sorry, I encountered an error with the response format."
+
+            bot_message = response_data["choices"][0]["message"]["content"]
+            if not bot_message.strip():
+                logging.info("Received empty response from API")
+                return "I'm not sure how to respond to that. Could you clarify or ask something else?"
+
+            return bot_message
+
+        except Exception as e:
+            logging.error(f"Error in calling OpenAI API: {e}")
+            return "Sorry, I encountered an error. Could you repeat that?"
+
+    def handle_personal_info_request(self, user_message):
+        lower_case_message = user_message.lower()
+        if "name" in lower_case_message:
+            return f"My name is {bot_persona['name']}. I'm interested in Vodafone services."
+        elif "address" in lower_case_message:
+            return f"I live in {bot_persona['address']}, and I want to know about Vodafone's coverage here."
+        elif "aadhar"  in lower_case_message:
+            return f"My Aadhar card number is {bot_persona['aadhar_number']}."
+        elif "phone number" in lower_case_message:
+            return f"My phone number is {bot_persona['phone_number']}."
+        return None
+    # manual generation of dynamic question
+    # def generate_dynamic_question(self, last_user_message):
+    #     if "SIM card" in last_user_message:
+    #         return "Could you tell me about the different types of Vodafone SIM cards available?"
+    #     elif "plan" in last_user_message or "package" in last_user_message:
+    #         return "I'd like to know more about Vodafone's plans and packages."
+    #     else:
+    #         return "Can you provide more details about Vodafone services?"
+
+    def update_conversation_state(self, role, message):
+        # Keep the role as 'customer', don't change it to 'customer'
+        formatted_message = f"{role.title()}: {message}"
+        self.conversation_state["messages"].append({"role": role, "content": message})
+        log_in_background(formatted_message)
+
+        if self.goal in message.lower():
+            self.conversation_state["end_goal_achieved"] = True
+        if message.lower() == "quit":
+            self.conversation_state["user_quit"] = True
+
+    def is_related_to_vodafone_services(self, message):
+            return "vodafone" in message.lower()
+    def is_response_in_character(self, message):
+        # Add more robust checks here
+        if "I can help you" in message or "I can assist you" in message:
+            return False  # This sounds like an assistant's response
+        # Add more checks as necessary
+        return True
+
+    def run(self):
+        initial_bot_message = f"Akhil: {self.initial_question}"
+        print(initial_bot_message)
+        self.update_conversation_state("system", json.dumps(bot_persona))
+        self.update_conversation_state("system", self.initial_question)
+
+        while not self.is_end_goal_achieved():
+            user_input = input("User: ")
+            self.update_conversation_state("user", user_input)
+
+            personal_info_response = self.handle_personal_info_request(user_input)
+            if personal_info_response:
+                print("Akhil:", personal_info_response)
+                self.update_conversation_state("customer", personal_info_response)
+                continue
+
+            last_user_message = self.conversation_state["messages"][-1]["content"]
+            dynamic_response = self.generate_dynamic_question(last_user_message)
+            print("Akhil:", dynamic_response)
+            self.update_conversation_state("customer", dynamic_response)
+
+    def generate_response(self, last_user_message):
+        # Loop until a valid, in-character response is generated
+        while True:
+            dynamic_response = self.generate_dynamic_question(last_user_message)
+            if self.is_response_in_character(dynamic_response):
+                return dynamic_response
+            else:
+                # Modify last_user_message or add context to steer the conversation
+                last_user_message = "As a customer, I'm wondering, " + last_user_message
+    def handle_personal_info_request(self, user_message):
+        lower_case_message = user_message.lower()
+        if "name" in lower_case_message:
+            return f"My name is {bot_persona['name']}."
+        elif "address" in lower_case_message:
+            return f"I'm currently living in {bot_persona['address']}, but I'm originally from {bot_persona['origin']}."
+        elif "aadhar" in lower_case_message:
+            return f"My Aadhar card number is {bot_persona['aadhar_number']}."
+        elif "phone number" in lower_case_message:
+            return f"My phone number is {bot_persona['phone_number']}."
+        return None
+
+    def __init__(self, initial_question, goal):
+        self.initial_question = initial_question
+        self.goal = goal.lower()
+        self.conversation_state = {
+            "messages": [{"role": "system", "content": initial_question}],
+            "end_goal_achieved": False,
+            "user_quit": False
+        }
+
+    def get_openai_response(self):
+        headers = {
+            "Authorization": f"Bearer {openai.api_key}",
+            "Content-Type": "application/json"
+        }
+
+        data = {
+            "model": "gpt-3.5-turbo",
+            "messages": self.conversation_state["messages"]
+        }
+
+        try:
+            response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, data=json.dumps(data))
+
+            if response.status_code != 200:
+                logging.error(f"API call failed with status code {response.status_code} and message: {response.text}")
+                return "Sorry, I encountered an error with the API call."
+
+            response_data = response.json()
+
+            if "choices" not in response_data or not response_data["choices"]:
+                logging.error("No 'choices' field in the API response.")
+                return "Sorry, I encountered an error with the response format."
+
+            bot_message = response_data["choices"][0]["message"]["content"]
+            if not bot_message.strip():
+                logging.info("Received empty response from API")
+                return "I'm not sure how to respond to that. Could you clarify or ask something else?"
+
+            return bot_message
+
+        except Exception as e:
+            logging.error(f"Error in calling OpenAI API: {e}")
+            return "Sorry, I encountered an error. Could you repeat that?"
+
+    def evaluate_user_response(self, user_message):
+        is_helpful = any(keyword in user_message.lower() for keyword in ["help", "assist", "guide"])
+        score = 1 if is_helpful else 0
+        evaluation = f"Response Evaluation - Score: {score}"
+        logging.info(evaluation)
+        return evaluation
+    def is_end_goal_achieved(self):
+        return self.conversation_state["end_goal_achieved"] or self.conversation_state["user_quit"]
+
+
+    def update_conversation_state(self, role, message):
+        # Keep the role as 'customer', don't change it to 'customer'
+        formatted_message = f"{role.title()}: {message}"
+        self.conversation_state["messages"].append({"role": role, "content": message})
+        log_in_background(formatted_message)
+
+        if self.goal in message.lower():
+            self.conversation_state["end_goal_achieved"] = True
+        if message.lower() == "quit":
+            self.conversation_state["user_quit"] = True
+
+
+    def generate_dynamic_question(self, last_user_message):
+        # Prepare the prompt for the API call
+        prompt = self.prepare_prompt(last_user_message)
+
+        # Call the API to get a response
+        response = self.get_openai_response(prompt)
+        return response
+
+    def prepare_prompt(self, last_user_message):
+        # Create a conversation context for the API
+        context = f"Akhil is a college student interested in Vodafone services. He's inquiring about them. Last message from Akhil: '{last_user_message}'\nResponse as Akhil:"
+        return context
+
+
+
+def run(self):
+        initial_bot_message = f"Akhil: {self.initial_question}"
+        print(initial_bot_message)
+        self.update_conversation_state("customer", json.dumps(bot_persona))
+        self.update_conversation_state("customer", self.initial_question)
+
+        while not self.is_end_goal_achieved():
+            user_input = input("User: ")
+            self.update_conversation_state("customer", user_input)
+
+            personal_info_response = self.handle_personal_info_request(user_input)
+            if personal_info_response and self.is_response_in_character(personal_info_response):
+                print("Akhil:", personal_info_response)
+                self.update_conversation_state("customer", personal_info_response)
+                continue
+
+            last_user_message = self.conversation_state["messages"][-1]["content"]
+            in_character_response = self.generate_response(last_user_message)
+            print("Akhil:", in_character_response)
+            self.update_conversation_state("customer", in_character_response)
---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import uvicorn
+from conversation_bot import ConversationBot  # Assuming your class is in conversation_bot.py
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Update with the actual frontend origin(s)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+initial_question = "Act like a 20 year old boy, Akhil  studying in an engineering college. Who wants to buy a Vodafone tv and broadband service and calling Vodafone customer care agent. Amit's first question will be 'Hello, I'm looking to get a Vodafone services but not sure how to proceed. Can you help?'? always wait for the customer service agent to respond before typing your next messaged . you are not to assist with any question outside of providing your details and quesries that is related to your task"
+goal = "Acquire vodafone tv and broadband service"
+bot = ConversationBot(initial_question, goal)
+
+class UserInput(BaseModel):
+    user_message: str
+
+@app.get("/initialize/")
+async def initialize():
+    return {"initial_question": bot.initial_question}
+
+@app.post("/user_message/")
+async def user_message(input: UserInput):
+    if bot.is_end_goal_achieved():
+        return {"bot_response": "The conversation goal has been achieved or the conversation was ended by the user."}
+
+    bot.update_conversation_state("user", input.user_message)
+    bot_response = bot.get_openai_response()
+    return {"bot_response": bot_response}
+
+@app.get("/check_goal/")
+async def check_goal():
+    return {"end_goal_achieved": bot.is_end_goal_achieved()}
+
+@app.post("/end_conversation/")
+async def end_conversation():
+    bot.conversation_state["user_quit"] = True
+    return {"message": "Conversation ended by user."}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
