550d7dcc24c9826616a633898e7fda1295ed88cb
Shreyaanp <shreyaan.pradhan2020@vitstudent.ac.in>
2023-11-25 02:48:52 +0530

    some changes made

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ async def end_conversation():
     return {"message": "Conversation ended by user."}
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
